059a048fb7c6633269fa9059a7cef41a2ed787bc
Travis Exline <t.exline22@gmail.com>
2019-07-07 14:23:47 +0000

    Automatically backed up by Learn

--- db/migrate/20190707142223_create_tweets.rb
+++ db/migrate/20190707142223_create_tweets.rb
@@ -1,4 +1,9 @@
 class CreateTweets < ActiveRecord::Migration
   def change
+    create_table :tweets do |t|
+      t.text :content
+      t.integer :user_id
+      t.timestamp null: false
+    end
   end
 end
